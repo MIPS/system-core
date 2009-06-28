9b4b0b055ce3e54b93b469cce9986f1ab9adb19a
Chris Dearman <chris@mips.com>
2009-06-28 03:26:17 -0700

    [MIPS] Initial support
    Signed-off-by: Chris Dearman <chris@mips.com>

--- init/parser.c
+++ init/parser.c
@@ -258,6 +258,7 @@ textresume:
             x++;
             goto textdone;
         case '\n':
+	    state->line++;
             state->nexttoken = T_NEWLINE;
             x++;
             goto textdone;
