1b84a6063ba3460d16b76ce3a1d70e7b24ceb4bc
Paul Lind <plind@mips.com>
2009-11-18 16:17:28 -0800

    MIPS pixelflinger initial release, merged to a single commit
    This release has some known bugs, described below. For this reason, the
    optimizations are disabled by default. We have plans to fix the bugs, and 
    continue with further performance optimizations.
    
    The optimizations can be turned on at runtime by setting a property:
    
        setprop debug.pf.asm 1
    
    they can be disabled at any time by:
    
        setprop debug.pf.asm 0
    
    For anyone wanting to look at the code that is generated, you can set this property:
    
        setprop debug.pf.disasm 1
    
    Known bugs
    ----------
    
    - Some error dialogs gray out the background screen, optimimixzed pixelflinger 
    incorrectly gives a red cast to this background.
    
    - There are texturing bugs, which show up as blurry/fuzzy rendering on 
    ApiDemos/Graphics/OpenGL/Sprite Text and shows as a crash on
    ApiDemos/Graphics/OpenGL/Textured-Triangle. I suspect most OpenGL 
    apps that use textures will not work.

--- libpixelflinger/tests/codegen/codegen.cpp
+++ libpixelflinger/tests/codegen/codegen.cpp
@@ -1,21 +1,38 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 extern "C" void ggl_test_codegen(
-        uint32_t n, uint32_t p, uint32_t t0, uint32_t t1);
+        int mode, uint32_t n, uint32_t p, uint32_t t0, uint32_t t1);
 
+void usage_exit(const char *name)
+{
+    printf("usage: %s a|m 00000117:03454504:00001501:00000000\n", name);
+    exit(1);
+}
 
 int main(int argc, char** argv)
 {
-    if (argc != 2) {
-        printf("usage: %s 00000117:03454504_00001501_00000000\n", argv[0]);
-        return 0;
+    if (argc != 3 || (argv[1][0] != 'a' && argv[1][0] != 'm'  && argv[1][0] != 'b')) {
+        usage_exit(argv[0]);
     }
+    int      mode, acnt;
     uint32_t n;
     uint32_t p;
     uint32_t t0;
     uint32_t t1;
-    sscanf(argv[1], "%08x:%08x_%08x_%08x", &p, &n, &t0, &t1);
-    ggl_test_codegen(n, p,  t0, t1);
+    if (argv[1][0] == 'a') mode = 1;
+    else if (argv[1][0] == 'm') mode = 2;
+    else mode = 3;  // both (mips with arm mixed)
+    acnt = sscanf(argv[2], "%08x:%08x:%08x:%08x", &n, &p, &t0, &t1);
+    if (acnt != 4) {
+        // OK, try the alternate format-order that the ARM code prints out....
+        // note that n & p are reversed in this one
+        acnt = sscanf(argv[2], "%08x:%08x_%08x_%08x", &p, &n, &t0, &t1);
+        if (acnt != 4) {
+            usage_exit(argv[0]);
+        }
+    }
+    ggl_test_codegen(mode, n, p, t0, t1);
     return 0;
 }
