f8ab0caae339ecdca99ff6190bab36ae958208b0
Chris Dearman <chris@mips.com>
2010-01-11 15:21:54 -0800

    Corrected line number counting in init parser
    Signed-off-by: Chris Dearman <chris@mips.com>

--- init/parser.c
+++ init/parser.c
@@ -262,6 +262,7 @@ textresume:
             x++;
             goto textdone;
         case '\n':
+	    state->line++;
             state->nexttoken = T_NEWLINE;
             x++;
             goto textdone;
