547f234f7d0d2cdec90a45325e45d8d257aff141
Chris Dearman <chris@mips.com>
2010-07-20 16:42:54 -0700

    [ENDIAN] ADB Endian fixes
    Signed-off-by: Chris Dearman <chris@mips.com>

--- adb/transport.c
+++ adb/transport.c
@@ -25,6 +25,21 @@
 #define   TRACE_TAG  TRACE_TRANSPORT
 #include "adb.h"
 
+#ifdef HAVE_BIG_ENDIAN
+#define H4(x)	(((x) & 0xFF000000) >> 24) | (((x) & 0x00FF0000) >> 8) | (((x) & 0x0000FF00) << 8) | (((x) & 0x000000FF) << 24)
+static inline void fix_endians(apacket *p)
+{
+    p->msg.command     = H4(p->msg.command);
+    p->msg.arg0        = H4(p->msg.arg0);
+    p->msg.arg1        = H4(p->msg.arg1);
+    p->msg.data_length = H4(p->msg.data_length);
+    p->msg.data_check  = H4(p->msg.data_check);
+    p->msg.magic       = H4(p->msg.magic);
+}
+#else
+#define fix_endians(p) do {} while (0)
+#endif
+
 static void transport_unref(atransport *t);
 
 static atransport transport_list = {
@@ -156,6 +171,8 @@ read_packet(int  fd, const char* name, apacket** ppacket)
         }
     }
 
+    fix_endians(*ppacket);
+
 #if ADB_TRACE
     if (ADB_TRACING) {
         dump_packet(name, "from remote", *ppacket);
@@ -180,6 +197,9 @@ write_packet(int  fd, const char* name, apacket** ppacket)
         dump_packet(name, "to remote", *ppacket);
     }
 #endif
+
+    fix_endians(*ppacket);
+
     len = sizeof(ppacket);
     while(len > 0) {
         r = adb_write(fd, p, len);
